a42432e715f8d549419ff107fbe42f27a9b38b57
projetmbc <projetmbc@gmail.com>
2025-06-19 10:06:48 +0200

    [multimd] Working on hatch \& RC 1st version #16

---
+++ multimd/tests/api/test_02_build.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from multimd import Builder, Path
+
+THIS_DIR = Path(__file__).parent
+DATA_DIR = THIS_DIR.parent / "data" / "buildit"
+TEMP_DIR = DATA_DIR / ".temp"
+
+def test_builder():
+    for final_MD in DATA_DIR.glob("*.md"):
+        src   = DATA_DIR / final_MD.stem
+        dest  = TEMP_DIR / final_MD.name
+
+        Builder(
+            src   = src,
+            dest  = dest,
+            erase = True,
+        ).build()
+
+        text_build    = dest.read_text().rstrip()
+        text_expected = final_MD.read_text().rstrip()
+
+        assert text_build == text_expected
