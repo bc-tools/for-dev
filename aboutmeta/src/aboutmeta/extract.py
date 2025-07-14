2209d62420fcfef395a0780fa608fdf6052323d3
projetmbc <projetmbc@gmail.com>
2025-07-14 21:21:40 +0200

    [aboutmeta] Start implementation #29

--- aboutmeta/src/aboutmeta/extract.py
+++ aboutmeta/src/aboutmeta/extract.py
@@ -38,15 +38,59 @@ class Extract:
         )
 
     def __recu_parse(self, data, specs):
+        data_parsed = {}
+
         for key, val in data.items():
+# Legal key?
             if not key in specs:
                 raise KeyError(f"unknown key ''{key}''.")
 
-            # subspecs =specs[key]
+# Good kind of data?
+            key_type = specs[key][TAG_SPECS_TYPE]
 
-            # __recu_parse(self, boxdata, specs)
+            needed = ""
 
+            match key_type:
+                case "BLOCK":
+                    if not isinstance(val, dict):
+                        needed = TAG_SPECS_BLOCK.lower()
 
+                case "DATA":
+                    if specs[key][TAG_SPECS_LIST_OF]:
+                        if not isinstance(val, list):
+                            needed = "list of data"
 
-# xtrct = Extract()
-# xtrct.build(Path(__file__).parent.parent.parent / "about.yaml")
+                    else:
+                        if not isinstance(val, str):
+                            needed = "a data"
+
+            if needed:
+                raise ValueError(
+                    f"content of ''{key}'' must be a {needed}."
+                )
+
+# Block data needs a recursive work.
+            if key_type == TAG_SPECS_BLOCK:
+                data_parsed[key] = self.__recu_parse(val,
+            specs[key][TAG_SPECS_CONTENT])
+
+# List of data needs an iterative parsing.
+            else:
+                parser = specs[key][TAG_SPECS_PARSER]
+
+                if specs[key][TAG_SPECS_LIST_OF]:
+                    for i, d in enumerate(val):
+                        val[i] = parser
+
+                    data_parsed[key] = val
+
+                else:
+                    data_parsed[key] = parser
+
+# Job done.
+        return data_parsed
+
+xtrct = Extract()
+xtrct.build(Path(__file__).parent.parent.parent / "about.yaml")
+
+from pprint import pprint;pprint(xtrct.data)
