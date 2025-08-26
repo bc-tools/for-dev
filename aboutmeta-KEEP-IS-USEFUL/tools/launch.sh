b3df0e261c34e6b38c31d1452ea166f4eaad5f48
projetmbc <projetmbc@gmail.com>
2025-08-27 00:37:54 +0200

    [aboutmeta] Integration of cb-py-dev copier template #5

--- aboutmeta-KEEP-IS-USEFUL/tools/launch.sh
+++ aboutmeta-KEEP-IS-USEFUL/tools/launch.sh
@@ -5,26 +5,25 @@
 # --------------- #
 
 THIS_DIR="$(cd "$(dirname "$0")" && pwd)"
+THIS_FILE=$(basename "$0")
+THIS_FILE=${THIS_FILE%%.*}
 
-THIS_NAME=$(basename "$0")
-THIS_STEM=${THIS_NAME%%.*}
 
+# -------------- #
+# -- AUTO-DOC -- #
+# -------------- #
 
-# ------------ #
-# -- MANUAL -- #
-# ------------ #
-
-USAGE="Usage: bash $THIS_NAME [OPTIONS]"
-TRY="'bash $THIS_NAME --help' for help."
+USAGE="Usage: bash $THIS_FILE.bash [OPTIONS]"
+TRY="'bash $THIS_FILE.bash --help' for help."
 
 HELP="$USAGE
 
-  Launch all project tools (coded in Python).
+  Launch all Python buider files.
 
 Options:
   -q, --quick Any builder named '...-slow.py' will be ignored.
               This option is useful during the development phase,
-              but NOT WHEN THE PROJECT HAS TO BE PUBLISHED.
+              but not when the project has to be published.
   -h, --help  Show this message and exit.
 "
 
@@ -33,22 +32,12 @@ Options:
 # -- TOOLS -- #
 # ----------- #
 
-###
-# prototype::
-#     #1 : an error code.
-#     #2 : a message.
-###
 print_cli_info() {
     echo "$2"
     exit $1
 }
 
 
-###
-# prototype::
-#     #1 : a directory path.
-#     #2 : a file name.
-###
 error_exit() {
     printf "\033[91m\033[1m"
 
@@ -59,11 +48,6 @@ error_exit() {
 }
 
 
-###
-# prototype::
-#     #1 : a terminal colour code.
-#     #2 : a message.
-###
 print_about() {
     printf "\033[$1"
     echo "$2"
@@ -115,15 +99,11 @@ fi
 # -- LET'S WORK! -- #
 # ----------------- #
 
-# Let's work locally.
 cd "$THIS_DIR"
 
-# Logging must be done in the `tools.log` file, and we erase it
-# at each launching.
 rm -f tools.log
 
-# We ignore any Python file inside the `cbutils` folder.
-find . -type f -name "*.py" ! -path "./cbutils/*" | sort | while read -r builderfile
+find . -type f -name "*.py" ! -path "./utilities/*" | sort | while read -r builderfile
 do
     echo ""
 
@@ -131,13 +111,10 @@ do
 
     if [[ $QUICKOPTION == 1 && $filename =~ ^.*-slow\.py ]]
     then
-# Yellow formatting.
         print_about "33m" "Ignoring $builderfile"
 
     else
-# Green formatting.
         print_about "32m" "Launching $builderfile"
-
         python "$builderfile" || error_exit "$THIS_DIR" "$builderfile"
     fi
 done
--- aboutmeta-old/tools/launch.sh
+++ aboutmeta-old/tools/launch.sh
@@ -5,25 +5,26 @@
 # --------------- #
 
 THIS_DIR="$(cd "$(dirname "$0")" && pwd)"
-THIS_FILE=$(basename "$0")
-THIS_FILE=${THIS_FILE%%.*}
+
+THIS_NAME=$(basename "$0")
+THIS_STEM=${THIS_NAME%%.*}
 
 
-# -------------- #
-# -- AUTO-DOC -- #
-# -------------- #
+# ------------ #
+# -- MANUAL -- #
+# ------------ #
 
-USAGE="Usage: bash $THIS_FILE.bash [OPTIONS]"
-TRY="'bash $THIS_FILE.bash --help' for help."
+USAGE="Usage: bash $THIS_NAME [OPTIONS]"
+TRY="'bash $THIS_NAME --help' for help."
 
 HELP="$USAGE
 
-  Launch all Python buider files.
+  Launch all project tools (coded in Python).
 
 Options:
   -q, --quick Any builder named '...-slow.py' will be ignored.
               This option is useful during the development phase,
-              but not when the project has to be published.
+              but NOT WHEN THE PROJECT HAS TO BE PUBLISHED.
   -h, --help  Show this message and exit.
 "
 
@@ -32,12 +33,22 @@ Options:
 # -- TOOLS -- #
 # ----------- #
 
+###
+# prototype::
+#     #1 : an error code.
+#     #2 : a message.
+###
 print_cli_info() {
     echo "$2"
     exit $1
 }
 
 
+###
+# prototype::
+#     #1 : a directory path.
+#     #2 : a file name.
+###
 error_exit() {
     printf "\033[91m\033[1m"
 
@@ -48,6 +59,11 @@ error_exit() {
 }
 
 
+###
+# prototype::
+#     #1 : a terminal colour code.
+#     #2 : a message.
+###
 print_about() {
     printf "\033[$1"
     echo "$2"
@@ -99,11 +115,15 @@ fi
 # -- LET'S WORK! -- #
 # ----------------- #
 
+# Let's work locally.
 cd "$THIS_DIR"
 
+# Logging must be done in the `tools.log` file, and we erase it
+# at each launching.
 rm -f tools.log
 
-find . -type f -name "*.py" ! -path "./utilities/*" | sort | while read -r builderfile
+# We ignore any Python file inside the `cbutils` folder.
+find . -type f -name "*.py" ! -path "./cbutils/*" | sort | while read -r builderfile
 do
     echo ""
 
@@ -111,10 +131,13 @@ do
 
     if [[ $QUICKOPTION == 1 && $filename =~ ^.*-slow\.py ]]
     then
+# Yellow formatting.
         print_about "33m" "Ignoring $builderfile"
 
     else
+# Green formatting.
         print_about "32m" "Launching $builderfile"
+
         python "$builderfile" || error_exit "$THIS_DIR" "$builderfile"
     fi
 done
