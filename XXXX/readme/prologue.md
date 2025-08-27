dfe7a9406e033777c95fa14701c39d284908491e
projetmbc <projetmbc@gmail.com>
2025-08-27 20:45:10 +0200

    [aboutmeta] Testing copier template #1

---
+++ XXXX/readme/prologue.md
@@ -0,0 +1,9 @@
+The Python module XXXX
+----------------------
+
+This document is a complete tutorial showing all the available features.
+
+
+**Table of contents**
+
+::TOC::
---
+++ XXXX/tools/cbutils/core/constants.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import re
+
+
+# ------------ #
+# -- README -- #
+# ------------ #
+
+TAG_README = 'readme'
+
+
+# ------------------- #
+# -- FOLDER / FILE -- #
+# ------------------- #
+
+TAG_FILE  = "file"
+
+
+# ----------------- #
+# -- ERROR & CO. -- #
+# ----------------- #
+
+TAG_WARNING  = "warning"
+TAG_CRITICAL = "critical"
+TAG_ERROR    = "error"
